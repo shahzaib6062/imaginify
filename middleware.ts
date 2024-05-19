90d3ff3a5d05c2dde462e7faa7f716a08bba1395
shahzaib6062 <shahxaib895@gmail.com>
2024-05-19 12:41:39 +0500

    added clerk api

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,9 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware({ publicRoutes: ["/api/webhooks/clerk"] });
+export default authMiddleware({
+  publicRoutes: ["/api/webhooks/clerk"],
+});
 
 export const config = {
-  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
